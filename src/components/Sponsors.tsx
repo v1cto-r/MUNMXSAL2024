4b249b63601eb12f79a93e946c7c622254117b57
Victor de la Rosa <delarosasanovic@gmail.com>
2024-04-04 12:37:37 -0600

    Added sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -6,8 +6,29 @@ function Sponsors () {
     <main>
       <div id="sponsors">
         <div id='intro'>
-          <h1>Sponsors Coming Soon...</h1>
+          <h1>Thank you to our Sponsors</h1>
         </div>
+        <section id="sponsors-logos" className='landing-section'>
+          <div className="row">
+            <div className="sponsor-img">
+              <img src="/sponsors/sponsor1.webp" alt="sponsor"/>
+            </div>
+            <div className="sponsor-img">
+              <img src="/sponsors/sponsor5.webp" alt="sponsor"/>
+            </div>
+            <div className="sponsor-img">
+              <img src="/sponsors/sponsor3.webp" alt="sponsor"/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="sponsor-img">
+              <img src="/sponsors/sponsor4.webp" alt="sponsor"/>
+            </div>
+            <div className="sponsor-img">
+              <img src="/sponsors/sponsor2.webp" alt="sponsor"/>
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   )
