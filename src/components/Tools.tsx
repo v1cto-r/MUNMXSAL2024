4ed991fc8ad732df1dad88b6489b315106186ee4
v1cto-r <delarosasanovic@gmail.com>
2024-04-16 16:21:34 -0600

    Added debate guide section in tools

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,6 +1,7 @@
 import data from '../data/links.json'
 import img1 from '/tools/img1.webp'
 import img2 from '/tools/img2.webp'
+import img3 from '/tools/img3.webp'
 import '../styles/Tools.css'
 import DoubleButton from './DoubleButton';
 
@@ -27,6 +28,14 @@ function Tools () {
             <h3>Parliamentary Procedure</h3>
             <DoubleButton url={jsonData.parliamentaryEN} text='English'/>
             <DoubleButton url={jsonData.parliamentaryES} text='Español'/>
+          </div>
+        </section>
+        <section className='tools-section'>
+          <img src={img3} alt='' id='parliamentary-img' className='tools-img'/>
+          <div className='info' id='position-paper'>
+            <h3>Debate Guides</h3>
+            <DoubleButton url={jsonData.guideEN} text='English'/>
+            <DoubleButton url={jsonData.guideES} text='Español'/>
           </div>
         </section>
       </div>
